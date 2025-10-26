5bdea02a8e4e750b07a5914cacd592010475be94
Fabiano <fabipiroli@live.com>
2025-10-26 20:12:02 -0300

    ajustes

---
+++ Scaffolding/Scaffolding/Models/Course.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Scaffolding.Models;
+
+public partial class Course
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public virtual ICollection<StudentCourse> StudentCourses { get; set; } = new List<StudentCourse>();
+}
---
+++ Veterinaria/Veterinaria/Migrations/20251026230601_Initial.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Veterinaria.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TiposAnimais",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Especie = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TiposAnimais", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Animais",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TipoAnimalId = table.Column<int>(type: "int", nullable: false),
+                    DtNascimento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Raca = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Cor = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Peso = table.Column<float>(type: "real", nullable: false),
+                    Altura = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animais", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Animais_TiposAnimais_TipoAnimalId",
+                        column: x => x.TipoAnimalId,
+                        principalTable: "TiposAnimais",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vacinas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DataAplicacao = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AnimalId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vacinas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Vacinas_Animais_AnimalId",
+                        column: x => x.AnimalId,
+                        principalTable: "Animais",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Animais_TipoAnimalId",
+                table: "Animais",
+                column: "TipoAnimalId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vacinas_AnimalId",
+                table: "Vacinas",
+                column: "AnimalId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vacinas");
+
+            migrationBuilder.DropTable(
+                name: "Animais");
+
+            migrationBuilder.DropTable(
+                name: "TiposAnimais");
+        }
+    }
+}
